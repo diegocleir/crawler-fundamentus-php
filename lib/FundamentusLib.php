915de9b42072e4501a83fc2fdc724074042390f8
Diego Alves <diegocleir@gmail.com>
2022-10-17 21:11:21 -0300

    incluida exportacao para xlsx no crawler

--- lib/FundamentusLib.php
+++ lib/FundamentusLib.php
@@ -2,7 +2,7 @@
 
 namespace lib;
 
-include_once('vendor/autoload.php');
+include_once(realpath( __DIR__ . '/../vendor/autoload.php'));
 include_once(realpath( __DIR__ . '/../model/fundamentus.php'));
 
 use DOMDocument;
@@ -69,36 +69,143 @@ class FundamentusLib
             $vacanciaMedia = ($tds->item(25)->nodeValue);
 
 
-            // $html = file_get_contents('http://www.fundamentus.com.br/detalhes.php?papel=' . $papel, false, $context);
+            $html = file_get_contents('http://www.fundamentus.com.br/detalhes.php?papel=' . $papel, false, $context);
         
-            // $internalErrors = libxml_use_internal_errors(true);
-            // $domDocument = new DOMDocument();
-            // $domDocument->loadHTML($html);
-            // libxml_use_internal_errors($internalErrors);
+            $internalErrors = libxml_use_internal_errors(true);
+            $domDocument = new DOMDocument();
+            $domDocument->loadHTML($html);
+            libxml_use_internal_errors($internalErrors);
 
-            // $tabelas = $domDocument->getElementsByTagName('table');
+            $tabelas = $domDocument->getElementsByTagName('table');
 
-            // $trs2 = $tabelas[0]->getElementsByTagName('tr');
+            $trs2 = $tabelas[0]->getElementsByTagName('tr');
 
-            // $tds2 = $trs2->item(1)->childNodes;
-            // $nome = $tds2->item(1)->nodeValue;
-            // $dataUltimaCotacao = $tds2->item(3)->nodeValue;
+            $tds2 = $trs2->item(1)->childNodes;
+            $nome = $tds2->item(3)->nodeValue;
+            $dataUltimaCotacao = $tds2->item(7)->nodeValue;
 
-            // $tds2 = $trs2->item(2)->childNodes;
-            // $mandato = $tds2->item(1)->nodeValue;
-            // $min52Semana = $tds2->item(3)->nodeValue;
+            $tds2 = $trs2->item(2)->childNodes;
+            $mandato = $tds2->item(3)->nodeValue;
+            $min52Semana = $tds2->item(7)->nodeValue;
 
-            // $tds2 = $trs2->item(3)->childNodes;
-            // $max52Semana = $tds2->item(3)->nodeValue;
+            $tds2 = $trs2->item(3)->childNodes;
+            $max52Semana = $tds2->item(7)->nodeValue;
 
-            // $tds2 = $trs2->item(2)->childNodes;
-            // $gestao = $tds2->item(1)->nodeValue;
-            // $volMedio2m = $tds2->item(3)->nodeValue;
+            $tds2 = $trs2->item(4)->childNodes;
+            $gestao = $tds2->item(3)->nodeValue;
+            $volMedio2m = $tds2->item(7)->nodeValue;
+
+            
+            $trs3 = $tabelas[1]->getElementsByTagName('tr');
+
+            $tds3 = $trs3->item(0)->childNodes;
+            $nCotas = $tds3->item(7)->nodeValue;
+
+            $tds3 = $trs3->item(1)->childNodes;
+            $relatorio = trim($tds3->item(3)->nodeValue);
+            $ultimoInfoTrimestral = $tds3->item(7)->nodeValue;
+
+
+            $trs4 = $tabelas[2]->getElementsByTagName('tr');
+
+            $tds4 = $trs4->item(1)->childNodes;
+            $ocilacoesDia = $tds4->item(3)->nodeValue;
+            $ffoCota = $tds4->item(11)->nodeValue;
+
+            $tds4 = $trs4->item(2)->childNodes;
+            $ocilacoesMes = $tds4->item(3)->nodeValue;
+            $ddyCota = $tds4->item(11)->nodeValue;
+
+            $tds4 = $trs4->item(3)->childNodes;
+            $ocilacoes30dias = $tds4->item(3)->nodeValue;
+            $vpCota = $tds4->item(11)->nodeValue;
+
+            $tds4 = $trs4->item(4)->childNodes;
+            $ocilacoes12meses = $tds4->item(3)->nodeValue;
+
+            $tds4 = $trs4->item(5)->childNodes;
+            $ocilacoes2022 = $tds4->item(3)->nodeValue;
+
+            $tds4 = $trs4->item(6)->childNodes;
+            $ocilacoes2021 = $tds4->item(3)->nodeValue;
+            $resultado12mesesReceita = $tds4->item(7)->nodeValue;
+            $resultado3mesesReceita = $tds4->item(11)->nodeValue;
+
+            $tds4 = $trs4->item(7)->childNodes;
+            $ocilacoes2020 = $tds4->item(3)->nodeValue;
+            $resultado12mesesVendaAtivos = $tds4->item(7)->nodeValue;
+            $resultado3mesesVendaAtivos = $tds4->item(11)->nodeValue;
+
+            $tds4 = $trs4->item(8)->childNodes;
+            $ocilacoes2019 = $tds4->item(3)->nodeValue;
+            $resultado12mesesFFO = $tds4->item(7)->nodeValue;
+            $resultado3mesesFFO = $tds4->item(11)->nodeValue;
+
+            $tds4 = $trs4->item(9)->childNodes;
+            $ocilacoes2018 = $tds4->item(3)->nodeValue;
+            $resultado12mesesRendDistribuido = $tds4->item(7)->nodeValue;
+            $resultado3mesesRendDistribuido = $tds4->item(11)->nodeValue;
+
+            $tds4 = $trs4->item(10)->childNodes;
+            $ocilacoes2017 = $tds4->item(3)->nodeValue;
+
+            $tds4 = $trs4->item(11)->childNodes;
+            $balancoPatrimonialAtivos = $tds4->item(7)->nodeValue;
+            $balancoPatrimonialPatrimonioLiquido = $tds4->item(11)->nodeValue;
+            
+            
+            $trs4 = $tabelas[4]->getElementsByTagName('tr');
+
+            $tds4 = $trs4->item(1)->childNodes;
+            $areaM2 = $tds4->item(7)->nodeValue;
+
+            $tds4 = $trs4->item(2)->childNodes;
+            $qtdeUnidades = $tds4->item(3)->nodeValue;
+
+            $tds4 = $trs4->item(3)->childNodes;
+            $imoveisPLFFI = $tds4->item(3)->nodeValue;
 
 
             $fundamentusDetalhes = new FundamentusDetalhes(
-                $papel
+                $papel,
+                $nome,
+                $dataUltimaCotacao,
+                $mandato,
+                $min52Semana,
+                $max52Semana,
+                $gestao,
+                $volMedio2m,
+                $nCotas,
+                $relatorio,
+                $ultimoInfoTrimestral,
+                $ocilacoesDia,
+                $ffoCota,
+                $ocilacoesMes,
+                $ddyCota,
+                $ocilacoes30dias,
+                $vpCota,
+                $ocilacoes12meses,
+                $ocilacoes2022,
+                $ocilacoes2021,
+                $resultado12mesesReceita,
+                $resultado3mesesReceita,
+                $ocilacoes2020,
+                $resultado12mesesVendaAtivos,
+                $resultado3mesesVendaAtivos,
+                $ocilacoes2019,
+                $resultado12mesesFFO,
+                $resultado3mesesFFO,
+                $ocilacoes2018,
+                $resultado12mesesRendDistribuido,
+                $resultado3mesesRendDistribuido,
+                $ocilacoes2017,
+                $balancoPatrimonialAtivos,
+                $balancoPatrimonialPatrimonioLiquido,
+                $areaM2,
+                $qtdeUnidades,
+                $imoveisPLFFI
             );
+
 
             $this->fundamentus[] = new Fundamentus(
                 $papel,
@@ -116,6 +223,7 @@ class FundamentusLib
                 $vacanciaMedia,
                 $fundamentusDetalhes
             );
+
         }
     }
 
--- model/Fundamentus.php
+++ model/Fundamentus.php
@@ -314,16 +314,41 @@ class FundamentusDetalhes
 {
     private $papel;
     private $nome;
+    private $dataUltimaCotacao;
     private $mandato;
-
-    /**
-     * FundamentusDetalhes constructor.
-     * @param $papel
-     */
-    public function __construct($papel)
-    {
-        $this->papel = $papel;
-    }
+    private $min52Semana;
+    private $max52Semana;
+    private $gestao;
+    private $volMedio2m;
+    private $nCotas;
+    private $relatorio;
+    private $ultimoInfoTrimestral;
+    private $ocilacoesDia;    
+    private $ffoCota;
+    private $ocilacoesMes;
+    private $ddyCota;
+    private $ocilacoes30dias;
+    private $vpCota;
+    private $ocilacoes12meses;
+    private $ocilacoes2022;
+    private $ocilacoes2021;
+    private $resultado12mesesReceita;
+    private $resultado3mesesReceita;
+    private $ocilacoes2020;
+    private $resultado12mesesVendaAtivos;
+    private $resultado3mesesVendaAtivos;
+    private $ocilacoes2019;
+    private $resultado12mesesFFO;
+    private $resultado3mesesFFO;
+    private $ocilacoes2018;
+    private $resultado12mesesRendDistribuido;
+    private $resultado3mesesRendDistribuido;
+    private $ocilacoes2017;
+    private $balancoPatrimonialAtivos;
+    private $balancoPatrimonialPatrimonioLiquido;
+    private $areaM2;
+    private $qtdeUnidades;
+    private $imoveisPLFFI;
 
     /**
      * @return mixed
@@ -335,12 +360,809 @@ class FundamentusDetalhes
 
     /**
      * @param mixed $papel
-     * @return Fundamentus
+     * @return FundamentusDetalhes
      */
     public function setPapel($papel)
     {
         $this->papel = $papel;
         return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     * @return FundamentusDetalhes
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDataUltimaCotacao()
+    {
+        return $this->dataUltimaCotacao;
+    }
+
+    /**
+     * @param mixed $dataUltimaCotacao
+     * @return FundamentusDetalhes
+     */
+    public function setDataUltimaCotacao($dataUltimaCotacao)
+    {
+        $this->dataUltimaCotacao = $dataUltimaCotacao;
+        return $this;
+    }
+    
+    /**
+     * Get the value of mandato
+     */ 
+    public function getMandato()
+    {
+        return $this->mandato;
+    }
+
+    /**
+     * Set the value of mandato
+     *
+     * @return  self
+     */ 
+    public function setMandato($mandato)
+    {
+        $this->mandato = $mandato;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of min52Semana
+     */ 
+    public function getMin52Semana()
+    {
+        return $this->min52Semana;
+    }
+
+    /**
+     * Set the value of min52Semana
+     *
+     * @return  self
+     */ 
+    public function setMin52Semana($min52Semana)
+    {
+        $this->min52Semana = $min52Semana;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of max52Semana
+     */ 
+    public function getMax52Semana()
+    {
+            return $this->max52Semana;
+    }
+
+    /**
+     * Set the value of max52Semana
+     *
+     * @return  self
+     */ 
+    public function setMax52Semana($max52Semana)
+    {
+            $this->max52Semana = $max52Semana;
+
+            return $this;
+    }
+
+    /**
+     * Get the value of gestao
+     */ 
+    public function getGestao()
+    {
+            return $this->gestao;
+    }
+
+    /**
+     * Set the value of gestao
+     *
+     * @return  self
+     */ 
+    public function setGestao($gestao)
+    {
+            $this->gestao = $gestao;
+
+            return $this;
+    }
+
+    /**
+     * Get the value of volMedio2m
+     */ 
+    public function getVolMedio2m()
+    {
+            return $this->volMedio2m;
+    }
+
+    /**
+     * Set the value of volMedio2m
+     *
+     * @return  self
+     */ 
+    public function setVolMedio2m($volMedio2m)
+    {
+            $this->volMedio2m = $volMedio2m;
+
+            return $this;
+    }
+
+    /**
+     * Get the value of nCotas
+     */ 
+    public function getNCotas()
+    {
+            return $this->nCotas;
+    }
+
+    /**
+     * Set the value of nCotas
+     *
+     * @return  self
+     */ 
+    public function setNCotas($nCotas)
+    {
+            $this->nCotas = $nCotas;
+
+            return $this;
+    }
+
+    /**
+     * Get the value of relatorio
+     */ 
+    public function getRelatorio()
+    {
+            return $this->relatorio;
+    }
+
+    /**
+     * Set the value of relatorio
+     *
+     * @return  self
+     */ 
+    public function setRelatorio($relatorio)
+    {
+            $this->relatorio = $relatorio;
+
+            return $this;
+    }
+
+    /**
+     * Get the value of ultimoInfoTrimestral
+     */ 
+    public function getUltimoInfoTrimestral()
+    {
+            return $this->ultimoInfoTrimestral;
+    }
+
+    /**
+     * Set the value of ultimoInfoTrimestral
+     *
+     * @return  self
+     */ 
+    public function setUltimoInfoTrimestral($ultimoInfoTrimestral)
+    {
+            $this->ultimoInfoTrimestral = $ultimoInfoTrimestral;
+
+            return $this;
+    }
+
+    /**
+     * Get the value of ocilacoesDia
+     */ 
+    public function getOcilacoesDia()
+    {
+            return $this->ocilacoesDia;
+    }
+
+    /**
+     * Set the value of ocilacoesDia
+     *
+     * @return  self
+     */ 
+    public function setOcilacoesDia($ocilacoesDia)
+    {
+            $this->ocilacoesDia = $ocilacoesDia;
+
+            return $this;
+    }
+
+    /**
+     * Get the value of ffoCota
+     */ 
+    public function getFfoCota()
+    {
+            return $this->ffoCota;
+    }
+
+    /**
+     * Set the value of ffoCota
+     *
+     * @return  self
+     */ 
+    public function setFfoCota($ffoCota)
+    {
+            $this->ffoCota = $ffoCota;
+
+            return $this;
+    }
+
+    /**
+     * Get the value of ocilacoesMes
+     */ 
+    public function getOcilacoesMes()
+    {
+        return $this->ocilacoesMes;
+    }
+
+    /**
+     * Set the value of ocilacoesMes
+     *
+     * @return  self
+     */ 
+    public function setOcilacoesMes($ocilacoesMes)
+    {
+        $this->ocilacoesMes = $ocilacoesMes;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of ddyCota
+     */ 
+    public function getDdyCota()
+    {
+        return $this->ddyCota;
+    }
+
+    /**
+     * Set the value of ddyCota
+     *
+     * @return  self
+     */ 
+    public function setDdyCota($ddyCota)
+    {
+        $this->ddyCota = $ddyCota;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of ocilacoes30dias
+     */ 
+    public function getOcilacoes30dias()
+    {
+        return $this->ocilacoes30dias;
+    }
+
+    /**
+     * Set the value of ocilacoes30dias
+     *
+     * @return  self
+     */ 
+    public function setOcilacoes30dias($ocilacoes30dias)
+    {
+        $this->ocilacoes30dias = $ocilacoes30dias;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of vpCota
+     */ 
+    public function getVpCota()
+    {
+        return $this->vpCota;
+    }
+
+    /**
+     * Set the value of vpCota
+     *
+     * @return  self
+     */ 
+    public function setVpCota($vpCota)
+    {
+        $this->vpCota = $vpCota;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of ocilacoes12meses
+     */ 
+    public function getOcilacoes12meses()
+    {
+        return $this->ocilacoes12meses;
+    }
+
+    /**
+     * Set the value of ocilacoes12meses
+     *
+     * @return  self
+     */ 
+    public function setOcilacoes12meses($ocilacoes12meses)
+    {
+        $this->ocilacoes12meses = $ocilacoes12meses;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of ocilacoes2022
+     */ 
+    public function getOcilacoes2022()
+    {
+        return $this->ocilacoes2022;
+    }
+
+    /**
+     * Set the value of ocilacoes2022
+     *
+     * @return  self
+     */ 
+    public function setOcilacoes2022($ocilacoes2022)
+    {
+        $this->ocilacoes2022 = $ocilacoes2022;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of ocilacoes2021
+     */ 
+    public function getOcilacoes2021()
+    {
+        return $this->ocilacoes2021;
+    }
+
+    /**
+     * Set the value of ocilacoes2021
+     *
+     * @return  self
+     */ 
+    public function setOcilacoes2021($ocilacoes2021)
+    {
+        $this->ocilacoes2021 = $ocilacoes2021;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of resultado12mesesReceita
+     */ 
+    public function getResultado12mesesReceita()
+    {
+        return $this->resultado12mesesReceita;
+    }
+
+    /**
+     * Set the value of resultado12mesesReceita
+     *
+     * @return  self
+     */ 
+    public function setResultado12mesesReceita($resultado12mesesReceita)
+    {
+        $this->resultado12mesesReceita = $resultado12mesesReceita;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of resultado3mesesReceita
+     */ 
+    public function getResultado3mesesReceita()
+    {
+        return $this->resultado3mesesReceita;
+    }
+
+    /**
+     * Set the value of resultado3mesesReceita
+     *
+     * @return  self
+     */ 
+    public function setResultado3mesesReceita($resultado3mesesReceita)
+    {
+        $this->resultado3mesesReceita = $resultado3mesesReceita;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of ocilacoes2020
+     */ 
+    public function getOcilacoes2020()
+    {
+        return $this->ocilacoes2020;
+    }
+
+    /**
+     * Set the value of ocilacoes2020
+     *
+     * @return  self
+     */ 
+    public function setOcilacoes2020($ocilacoes2020)
+    {
+        $this->ocilacoes2020 = $ocilacoes2020;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of resultado12mesesVendaAtivos
+     */ 
+    public function getResultado12mesesVendaAtivos()
+    {
+        return $this->resultado12mesesVendaAtivos;
+    }
+
+    /**
+     * Set the value of resultado12mesesVendaAtivos
+     *
+     * @return  self
+     */ 
+    public function setResultado12mesesVendaAtivos($resultado12mesesVendaAtivos)
+    {
+        $this->resultado12mesesVendaAtivos = $resultado12mesesVendaAtivos;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of resultado3mesesVendaAtivos
+     */ 
+    public function getResultado3mesesVendaAtivos()
+    {
+        return $this->resultado3mesesVendaAtivos;
+    }
+
+    /**
+     * Set the value of resultado3mesesVendaAtivos
+     *
+     * @return  self
+     */ 
+    public function setResultado3mesesVendaAtivos($resultado3mesesVendaAtivos)
+    {
+        $this->resultado3mesesVendaAtivos = $resultado3mesesVendaAtivos;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of ocilacoes2019
+     */ 
+    public function getOcilacoes2019()
+    {
+        return $this->ocilacoes2019;
+    }
+
+    /**
+     * Set the value of ocilacoes2019
+     *
+     * @return  self
+     */ 
+    public function setOcilacoes2019($ocilacoes2019)
+    {
+        $this->ocilacoes2019 = $ocilacoes2019;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of resultado12mesesFFO
+     */ 
+    public function getResultado12mesesFFO()
+    {
+        return $this->resultado12mesesFFO;
+    }
+
+    /**
+     * Set the value of resultado12mesesFFO
+     *
+     * @return  self
+     */ 
+    public function setResultado12mesesFFO($resultado12mesesFFO)
+    {
+        $this->resultado12mesesFFO = $resultado12mesesFFO;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of resultado3mesesFFO
+     */ 
+    public function getResultado3mesesFFO()
+    {
+        return $this->resultado3mesesFFO;
+    }
+
+    /**
+     * Set the value of resultado3mesesFFO
+     *
+     * @return  self
+     */ 
+    public function setResultado3mesesFFO($resultado3mesesFFO)
+    {
+        $this->resultado3mesesFFO = $resultado3mesesFFO;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of ocilacoes2018
+     */ 
+    public function getOcilacoes2018()
+    {
+        return $this->ocilacoes2018;
+    }
+
+    /**
+     * Set the value of ocilacoes2018
+     *
+     * @return  self
+     */ 
+    public function setOcilacoes2018($ocilacoes2018)
+    {
+        $this->ocilacoes2018 = $ocilacoes2018;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of resultado12mesesRendDistribuido
+     */ 
+    public function getResultado12mesesRendDistribuido()
+    {
+        return $this->resultado12mesesRendDistribuido;
+    }
+
+    /**
+     * Set the value of resultado12mesesRendDistribuido
+     *
+     * @return  self
+     */ 
+    public function setResultado12mesesRendDistribuido($resultado12mesesRendDistribuido)
+    {
+        $this->resultado12mesesRendDistribuido = $resultado12mesesRendDistribuido;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of resultado3mesesRendDistribuido
+     */ 
+    public function getResultado3mesesRendDistribuido()
+    {
+        return $this->resultado3mesesRendDistribuido;
+    }
+
+    /**
+     * Set the value of resultado3mesesRendDistribuido
+     *
+     * @return  self
+     */ 
+    public function setResultado3mesesRendDistribuido($resultado3mesesRendDistribuido)
+    {
+        $this->resultado3mesesRendDistribuido = $resultado3mesesRendDistribuido;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of ocilacoes2017
+     */ 
+    public function getOcilacoes2017()
+    {
+        return $this->ocilacoes2017;
+    }
+
+    /**
+     * Set the value of ocilacoes2017
+     *
+     * @return  self
+     */ 
+    public function setOcilacoes2017($ocilacoes2017)
+    {
+        $this->ocilacoes2017 = $ocilacoes2017;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of balancoPatrimonialAtivos
+     */ 
+    public function getBalancoPatrimonialAtivos()
+    {
+        return $this->balancoPatrimonialAtivos;
+    }
+
+    /**
+     * Set the value of balancoPatrimonialAtivos
+     *
+     * @return  self
+     */ 
+    public function setBalancoPatrimonialAtivos($balancoPatrimonialAtivos)
+    {
+        $this->balancoPatrimonialAtivos = $balancoPatrimonialAtivos;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of balancoPatrimonialPatrimonioLiquido
+     */ 
+    public function getBalancoPatrimonialPatrimonioLiquido()
+    {
+        return $this->balancoPatrimonialPatrimonioLiquido;
+    }
+
+    /**
+     * Set the value of balancoPatrimonialPatrimonioLiquido
+     *
+     * @return  self
+     */ 
+    public function setBalancoPatrimonialPatrimonioLiquido($balancoPatrimonialPatrimonioLiquido)
+    {
+        $this->balancoPatrimonialPatrimonioLiquido = $balancoPatrimonialPatrimonioLiquido;
+
+        return $this;
+    }
+
+    
+
+    /**
+     * Get the value of areaM2
+     */ 
+    public function getAreaM2()
+    {
+        return $this->areaM2;
+    }
+
+    /**
+     * Set the value of areaM2
+     *
+     * @return  self
+     */ 
+    public function setAreaM2($areaM2)
+    {
+        $this->areaM2 = $areaM2;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of qtdeUnidades
+     */ 
+    public function getQtdeUnidades()
+    {
+        return $this->qtdeUnidades;
+    }
+
+    /**
+     * Set the value of qtdeUnidades
+     *
+     * @return  self
+     */ 
+    public function setQtdeUnidades($qtdeUnidades)
+    {
+        $this->qtdeUnidades = $qtdeUnidades;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of imoveisPLFFI
+     */ 
+    public function getImoveisPLFFI()
+    {
+        return $this->imoveisPLFFI;
+    }
+
+    /**
+     * Set the value of imoveisPLFFI
+     *
+     * @return  self
+     */ 
+    public function setImoveisPLFFI($imoveisPLFFI)
+    {
+        $this->imoveisPLFFI = $imoveisPLFFI;
+
+        return $this;
+    }
+
+    /**
+     * FundamentusDetalhes constructor.
+     * @param $papel
+     */
+    public function __construct($papel, $nome, 
+        $dataUltimaCotacao, $mandato,
+        $min52Semana,
+        $max52Semana,
+        $gestao,
+        $volMedio2m,
+        $nCotas,
+        $relatorio,
+        $ultimoInfoTrimestral,
+        $ocilacoesDia,
+        $ffoCota,
+        $ocilacoesMes,
+        $ddyCota,
+        $ocilacoes30dias,
+        $vpCota,
+        $ocilacoes12meses,
+        $ocilacoes2022,
+        $ocilacoes2021,
+        $resultado12mesesReceita,
+        $resultado3mesesReceita,
+        $ocilacoes2020,
+        $resultado12mesesVendaAtivos,
+        $resultado3mesesVendaAtivos,
+        $ocilacoes2019,
+        $resultado12mesesFFO,
+        $resultado3mesesFFO,
+        $ocilacoes2018,
+        $resultado12mesesRendDistribuido,
+        $resultado3mesesRendDistribuido,
+        $ocilacoes2017,
+        $balancoPatrimonialAtivos,
+        $balancoPatrimonialPatrimonioLiquido,
+        $areaM2,
+        $qtdeUnidades,
+        $imoveisPLFFI)
+    {
+        $this->papel = $papel;
+        $this->nome = $nome;
+        $this->dataUltimaCotacao = $dataUltimaCotacao;
+        $this->mandato = $mandato;
+        $this->min52Semana = $min52Semana;
+        $this->max52Semana = $max52Semana;
+        $this->gestao = $gestao;
+        $this->volMedio2m = $volMedio2m;
+        $this->nCotas = $nCotas;
+        $this->relatorio = $relatorio;
+        $this->ultimoInfoTrimestral = $ultimoInfoTrimestral;
+        $this->ocilacoesDia = $ocilacoesDia;
+        $this->ffoCota = $ffoCota;
+        $this->ocilacoesMes = $ocilacoesMes;
+        $this->ddyCota = $ddyCota;
+        $this->ocilacoes30dias = $ocilacoes30dias;
+        $this->vpCota = $vpCota;
+        $this->ocilacoes12meses = $ocilacoes12meses;
+        $this->ocilacoes2022 = $ocilacoes2022;
+        $this->ocilacoes2021 = $ocilacoes2021;
+        $this->resultado12mesesReceita = $resultado12mesesReceita;
+        $this->resultado3mesesReceita = $resultado3mesesReceita;
+        $this->ocilacoes2020 = $ocilacoes2020;
+        $this->resultado12mesesVendaAtivos = $resultado12mesesVendaAtivos;
+        $this->resultado3mesesVendaAtivos = $resultado3mesesVendaAtivos;
+        $this->ocilacoes2019 = $ocilacoes2019;
+        $this->resultado12mesesFFO = $resultado12mesesFFO;
+        $this->resultado3mesesFFO = $resultado3mesesFFO;
+        $this->ocilacoes2018 = $ocilacoes2018;
+        $this->resultado12mesesRendDistribuido = $resultado12mesesRendDistribuido;
+        $this->resultado3mesesRendDistribuido = $resultado3mesesRendDistribuido;
+        $this->ocilacoes2017 = $ocilacoes2017;
+        $this->balancoPatrimonialAtivos = $balancoPatrimonialAtivos;
+        $this->balancoPatrimonialPatrimonioLiquido = $balancoPatrimonialPatrimonioLiquido;
+        $this->areaM2 = $areaM2;
+        $this->qtdeUnidades = $qtdeUnidades;
+        $this->imoveisPLFFI = $imoveisPLFFI;
     }
 
 }
